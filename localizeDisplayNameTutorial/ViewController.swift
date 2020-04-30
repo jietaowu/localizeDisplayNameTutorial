4c59b46230f1418fdd1e7aa1faaaa9dd28db2692
jietaowu <jietaowu2014@gmail.com>
2020-04-30 16:10:54 -0400

    Initial Commit

---
+++ localizeDisplayNameTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  localizeDisplayNameTutorial
+//
+//  Created by jwu on 4/30/20.
+//  Copyright © 2020 jwu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
